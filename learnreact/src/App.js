d4021c9732462eb24173a0539a7a12af2db8a393
roshankhaire <roshankhaire1992@gmail.com>
2022-10-21 17:24:34 -0700

    reactdemo

---
+++ learnreact/src/App.js
@@ -0,0 +1,28 @@
+
+
+import ExpenseItem from './components/ExpenseItem';
+
+
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+       
+          Learn React
+          <p>This is also working</p>
+          <ExpenseItem></ExpenseItem>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
